5329ee56d03aed044d1180743b8f9842a56c881f
Joseph McEnroe <108236812+JosephMcEnroe@users.noreply.github.com>
2023-12-11 11:25:54 -0500

    Files downloaded and uploaded

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn Python</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a class="nav-logo" href="index.php">Learn Python</a>
+            <ul class="nav-menu">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="tutorials.php">Tutorials</a></li>
+                <li class="nav-item"><a class="nav-link" href="quiz.php">Quiz</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="feedback.php">Feedback</a></li>
+                <li class="nav-item"><a class="nav-link" href="resources.php">Resources</a></li>
+            </ul>
+            <div class="hamburger-menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
+
+    <script src="./assets/js/mobilemenu.js" defer></script>
+
+</body>
+
+</html>
