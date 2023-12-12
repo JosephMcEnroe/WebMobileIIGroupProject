2f47aa907063a5fd167acbbb4cb6167107b47fee
Joseph McEnroe <108236812+JosephMcEnroe@users.noreply.github.com>
2023-12-12 11:28:09 -0500

    update to file system

---
+++ assets/presets/headerindex.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn Python</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<header>
+        <nav>
+            <a class="nav-logo" href="index.php">Learn Python</a>
+            <ul class="nav-menu">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="content/tutorials.php">Tutorials</a></li>
+                <li class="nav-item"><a class="nav-link" href="content/quiz.php">Quiz</a></li>
+                <li class="nav-item"><a class="nav-link" href="content/about.php">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="content/feedback.php">Feedback</a></li>
+                <li class="nav-item"><a class="nav-link" href="content/resources.php">Resources</a></li>
+            </ul>
+            <div class="hamburger-menu">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header>
---
+++ content/tutorials.php
@@ -0,0 +1,2 @@
+<?php include '../assets/presets/headercontent.php';?>
+<?php include '../assets/presets/footer.php';?>
