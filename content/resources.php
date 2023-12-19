23a16c20b2b8a4f6008fcdb47f180d8a534e7216
Truong Anh Tuan Hoang (RIT Student) <tth6714@ist-solace.main.ad.rit.edu>
2023-12-18 22:03:47 -0500

    add references

--- content/resources.php
+++ content/resources.php
@@ -1,4 +1,18 @@
 <?php 
 include '../assets/presets/header/header.php';
+?>
+
+
+<div class="container">
+<div class="content-center">
+<h1>References</h1> 
+<object data= 
+    "../assets/GroupProject.pdf" 
+    width="800"
+    height="500"> 
+</object> 
+</div>
+</div>
+<?php
 include '../assets/presets/footer/footer.php';
 ?>
